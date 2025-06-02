914b29916f456b12484728f08b266d9f3917818e
ZahwaReziFY <zahwarezi416@gmail.com>
2025-06-02 18:46:32 +0700

    add int main dan last commit noPewarisan

--- ParadigmaOOP/noPewarisan.cpp
+++ ParadigmaOOP/noPewarisan.cpp
@@ -18,3 +18,11 @@ class derivedClass : public baseClass
         cout << "Hallo saya Function dari derived class";
     }
 };
+
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
