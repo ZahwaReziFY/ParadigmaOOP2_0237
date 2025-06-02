0d1941025af29dc9e1564f1c0d9392e8b9f6944b
ZahwaReziFY <zahwarezi416@gmail.com>
2025-06-02 18:17:29 +0700

    membuat class abstraksi dan void setXY

--- ParadigmaOOP/abstraksi.cpp
+++ ParadigmaOOP/abstraksi.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class AbstraksiKlas
+{
+    private:
+    string x, y;
+
+    public:
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
+
+    
+}
